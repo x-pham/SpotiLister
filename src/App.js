06141162330210ec303b183b99bf73f80613cf98
x-pham <xpham@purdue.edu>
2022-10-21 21:05:36 -0400

    Add keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import keys from './keys.json';
+
 
 function App() {
+  var token;
+  var client_id = keys.client_id;
+  var client_secret = keys.client_secret;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="body">
+      <div class="playlistDisplay">
+        Hello
+      </div>
     </div>
   );
 }
